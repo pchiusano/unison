064abf77ed92a121154bc1ff8c7a0694b6ee6b36
Paul Chiusano <paul.chiusano@gmail.com>
2020-10-25 16:55:28 -0400

    More float functions

--- parser-typechecker/src/Unison/Util/Array.hs
+++ parser-typechecker/src/Unison/Util/Array.hs
@@ -8,6 +8,12 @@ module Unison.Util.Array (
   natReplicate,
   intReplicate,
   floatReplicate,
+  floatPtLt,
+  floatPtGt,
+  floatPtGteq,
+  floatPtLteq,
+  floatPtEq,
+  floatSwitch,
   boolReplicate,
   size,
   unsafeAt) 
@@ -22,7 +28,7 @@ data Array ix a where
   N :: !(A.Array A.D ix Word64) -> A.Array A.U ix Word64 -> Array ix Word64
   I :: !(A.Array A.D ix Int64) -> A.Array A.U ix Int64 -> Array ix Int64
   F :: !(A.Array A.D ix Double) -> A.Array A.U ix Double -> Array ix Double
-  B :: !(A.Array A.D ix Bit) -> A.Array A.U ix Bit -> Array ix Bit
+  B :: !(A.Array A.D ix Bit) -> A.Array A.U ix Bit -> Array ix Bool
 
 force :: A.Index ix => Array ix a -> Array ix a
 force (N _ af) = N (A.map id af) af
@@ -34,13 +40,13 @@ unsafeAt :: A.Index ix => ix -> Array ix a -> a
 unsafeAt ix (N _ af) = A.index' af ix
 unsafeAt ix (I _ af) = A.index' af ix
 unsafeAt ix (F _ af) = A.index' af ix
-unsafeAt ix (B _ af) = A.index' af ix
+unsafeAt ix (B _ af) = unBit $ A.index' af ix
 
 at :: A.Index ix => ix -> Array ix a -> Maybe a
 at ix (N _ af) = A.index af ix
 at ix (I _ af) = A.index af ix
 at ix (F _ af) = A.index af ix
-at ix (B _ af) = A.index af ix
+at ix (B _ af) = unBit <$> A.index af ix
 
 size :: A.Index ix => Array ix a -> ix
 size (N _ af) = A.unSz $ A.size af
@@ -51,19 +57,44 @@ size (B _ af) = A.unSz $ A.size af
 natReplicate :: A.Index ix => ix -> Word64 -> Array ix Word64
 natReplicate ix n = 
   let !a = A.makeArray A.Seq (A.Sz ix) (const n) 
-  in N (A.map id a) a
+  in N (delay a) a
 
 intReplicate :: A.Index ix => ix -> Int64 -> Array ix Int64
 intReplicate ix n = 
   let !a = A.makeArray A.Seq (A.Sz ix) (const n) 
-  in I (A.map id a) a
+  in I (delay a) a
 
 floatReplicate :: A.Index ix => ix -> Double -> Array ix Double
 floatReplicate ix n = 
   let !a = A.makeArray A.Seq (A.Sz ix) (const n) 
-  in F (A.map id a) a
+  in F (delay a) a
 
-boolReplicate :: A.Index ix => ix -> Bool -> Array ix Bit
+-- Pointwise less-than
+floatPtLt :: A.Index ix => Array ix Double -> Array ix Double -> Array ix Bool
+floatPtLt (F a1 _) (F a2 _) = let !a3 = A.map Bit (A.zipWith (<) a1 a2) in B a3 (A.compute a3)
+
+floatPtLteq :: A.Index ix => Array ix Double -> Array ix Double -> Array ix Bool
+floatPtLteq (F a1 _) (F a2 _) = let !a3 = A.map Bit (A.zipWith (<=) a1 a2) in B a3 (A.compute a3)
+
+floatPtGteq :: A.Index ix => Array ix Double -> Array ix Double -> Array ix Bool
+floatPtGteq (F a1 _) (F a2 _) = let !a3 = A.map Bit (A.zipWith (>=) a1 a2) in B a3 (A.compute a3)
+
+floatPtGt :: A.Index ix => Array ix Double -> Array ix Double -> Array ix Bool
+floatPtGt (F a1 _) (F a2 _) = let !a3 = A.map Bit (A.zipWith (>) a1 a2) in B a3 (A.compute a3)
+
+floatPtEq :: A.Index ix => Array ix Double -> Array ix Double -> Array ix Bool
+floatPtEq (F a1 _) (F a2 _) = let !a3 = A.map Bit (A.zipWith (==) a1 a2) in B a3 (A.compute a3)
+
+floatSwitch :: A.Index ix => Array ix Bool -> Array ix Double -> Array ix Double -> Array ix Double
+floatSwitch (B b _) (F a1 _) (F a2 _) = 
+  let !a = A.zipWith3 (\(Bit b) a1 a2 -> if b then a1 else a2) b a1 a2 
+  in F a (A.compute a)
+
+boolReplicate :: A.Index ix => ix -> Bool -> Array ix Bool
 boolReplicate ix n = 
   let !a = A.makeArray A.Seq (A.Sz ix) (const (Bit n))
-  in B (A.map id a) a
+  in B (delay a) a
+
+delay :: A.Source sh ix a => A.Index ix => A.Array sh ix a -> A.Array A.D ix a
+delay = A.map id
+{-# INLINE delay #-}
